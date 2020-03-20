18618e35699b2785c8595c0bd65c7ffae2996e5c
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-03-21 00:46:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let configArray = {};
+    bracketsConfig.forEach(bracket => {
+        configArray[bracket[0]] = bracket[1];
+    });
+
+    str = str.split('');
+    let i;
+
+    do {
+        str.some((bracket, index) => {
+            if (configArray[bracket] == str[index + 1]) {
+                i = index;
+                return true;
+            }
+
+            i = -1;
+        });
+
+        if (i != -1) {
+            str.splice(i, 2);
+        }
+    }
+    while (i != -1 && str.length > 1);
+
+    return str.length == 0;
 }
